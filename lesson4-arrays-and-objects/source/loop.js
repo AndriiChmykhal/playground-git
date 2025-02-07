f6ee1e1e45cd2c031184e3480d74de8c17d9943b
AndriiChmykhal <132781797+AndriiChmykhal@users.noreply.github.com>
2025-02-07 21:36:22 +0000

    homework 4 (#4)

---
+++ lesson4-arrays-and-objects/source/loop.js
@@ -0,0 +1,16 @@
+
+function loopOne() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+}
+
+loopOne();
+
+function loopTen() {
+    for (let i = 100; i >= 0; i -= 10) {
+        console.log(i);
+    }
+}
+
+loopTen();
